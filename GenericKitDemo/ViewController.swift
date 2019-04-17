c97e13e4130c40d5171ad3dd21aee1addc029cb6
Victor Marcias <victor@dyna.mo>
2019-04-17 11:33:29 +0200

    Initial commit.

---
+++ GenericKitDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GenericKit
+//
+//  Created by Victor Marcias on 2019-04-17.
+//  Copyright © 2019 Victor Marcias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
